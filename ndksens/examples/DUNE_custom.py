7ca303745ed5e2b03b1789c39fc666fdc8c3d290
Michel_Sorel <sorel@flxi02.fnal.gov>
2016-01-04 04:41:04 -0600

    Initial import of ndksens

---
+++ ndksens/examples/DUNE_custom.py
@@ -0,0 +1,33 @@
+###
+
+import os.path
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = FILE_PATH + '/../data/'
+import sys
+sys.path.append(FILE_PATH + '/..')
+
+import csv
+from numpy import arange
+from ndksens import units
+from ndksens import source
+from ndksens import mode
+from ndksens import experiment
+from ndksens import conflimits
+
+FCM = conflimits.FCMemoizer(0.9)
+FCM.ReadTableAverageUpperLimits(DATA_PATH+'FC90.dat')
+
+name = 'DUNE_custom'
+source = source.Ar
+mass = 40*units.kton
+eff = 0.97
+bgr = 1./(units.Mton*units.year)
+DUNE = experiment.Experiment(name, source, mass, mode.p19, eff, bgr)
+filename = 'dune_custom.dat'
+writer = csv.writer(open(filename, 'w'))
+
+for expo in arange(5.,405.,5.):
+	expo = expo * units.kton * units.year
+	sens = DUNE.sensitivity(expo,FCM)
+	print expo/(units.kton*units.year), sens/units.year
+	writer.writerow([expo/(units.kton*units.year), sens/units.year])
